c42d68a18ad9d47da97696a427cff1a6a369fbb7
Rodolfo <118237273+rodysapa@users.noreply.github.com>
2024-10-28 02:15:52 -0300

    Update validacao.js

--- validacao.js
+++ validacao.js
@@ -4,7 +4,6 @@ import bodyParser from 'body-parser';
 const app = express();
 const port = 4000;
 
-// Middleware para analisar o corpo das requisições no formato JSON
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
@@ -12,16 +11,13 @@ app.get('/', (req, res) => {
   res.send('Servidor está funcionando');
 });
 
-// Rota POST para validação de usuário e senha
 app.post('/login', (req, res) => {
   const { usuario, senha } = req.body;
 
-  // Verifica se os parâmetros foram fornecidos
   if (!usuario || !senha) {
     return res.send('Por favor, forneça um usuário e uma senha.');
   }
 
-  // Verifica se a senha é duas vezes o nome do usuário
   if (senha === usuario + usuario) {
     res.send('Usuário possui permissão de acesso.');
   } else {
@@ -29,7 +25,6 @@ app.post('/login', (req, res) => {
   }
 });
 
-// Inicializa o servidor
 app.listen(port, () => {
   console.log(`Servidor rodando em http://localhost:${port}`);
 });
